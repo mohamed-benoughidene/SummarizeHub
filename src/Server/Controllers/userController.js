e7e43a058c2434e4f27b2ccb624b0507e7bb7359
mohamed <91436391+mohamed-benoughidene@users.noreply.github.com>
2023-12-06 22:54:12 +0100

    Update userController.js

--- src/Server/Controllers/userController.js
+++ src/Server/Controllers/userController.js
@@ -2,7 +2,7 @@ const userModule = require('../module/userModule');
 require('dotenv').config();
 
 const jwt = require('jsonwebtoken');
-const Secret = process.env.SECRET_KEY; 
+const Secret = "gy3P6kCN9@!fT2$Z"; 
 // Define a function called generateToken that takes in an _id parameter
 function generateToken(_id){
         // Generate a token by signing the _id with the Secret
