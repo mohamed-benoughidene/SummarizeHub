5db727ec21516dc2e07ad81afdd012ce45c4fa4f
mohamed <91436391+mohamed-benoughidene@users.noreply.github.com>
2023-12-06 22:50:12 +0100

    Update requireAuth.js

--- src/Server/Middleware/requireAuth.js
+++ src/Server/Middleware/requireAuth.js
@@ -3,14 +3,14 @@ const userModule = require('../module/userModule')
 require("dotenv").config();
 async function requireAuth(req,res,next){
  const { authorization } = req.headers;
-
+ const Secret = process.env.SECRET_KEY; 
 if(!authorization){
     res.status(401).send({error:"you must be logged in"});
 }
 const token = authorization.split(" ")[1]
 
 try{
-const {_id} = jwt.verify(token, process.env.SECRET_KEY)
+const {_id} = jwt.verify(token, Secret)
  req.user = await userModule.findOne({_id});
  next()
 }catch(err){
