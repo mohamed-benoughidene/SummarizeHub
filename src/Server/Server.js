752d0ad7922f8ca3bc4681f55e1bd8bb48c10242
mohamed <91436391+mohamed-benoughidene@users.noreply.github.com>
2023-12-13 19:20:21 +0100

    Update Server.js

--- src/Server/Server.js
+++ src/Server/Server.js
@@ -6,12 +6,13 @@ require('dotenv').config();
 app.use(express.json());
 const port = process.env.PORT;
 const DB_Key = process.env.DB_KEY;
+ const SECRET_KEY = process.env.SECRET;
 const userRoutes = require('./Routes/UserRoutes');
 const SummarizerRoutes = require('./Routes/SummarizerRoutes');
 app.use(cors());
 app.listen(port);
 mongoose.connect(DB_Key).then(()=>{
-       console.log("server running");   
+       console.log("server running" + SECRET_KEY);   
 app.use('/api', userRoutes);
 app.use('/api', SummarizerRoutes);
     }).catch(err=>{ console.log("error:" + err.message)});
